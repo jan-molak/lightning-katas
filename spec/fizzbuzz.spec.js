6e447c3743c81f297ca9004188c0512c2d884154
awashbrook <awashbrook@gmail.com>
2014-09-23 10:01:42 +0100

    Thanks Apisett :) Thanks Apisett :)

--- spec/fizzbuzz.spec.js
+++ spec/fizzbuzz.spec.js
@@ -10,6 +10,22 @@ describe('"fizzbuzz" function', function () {
     });
 
     // exercise 1: return "Buzz" when called with a number divisible by 5
+    it('returns "Buzz" when called with a number divisible by 5', function () {
+        expect(fizzbuzz(5)).to.equal('Buzz');
+    });
 
     // exercise 2: return "FizzBuzz" when called with a number divisible by both 3 and 5
+    it('returns "FizzBuzz" when called with a number divisible by 3 and 15', function () {
+        expect(fizzbuzz(15)).to.equal('FizzBuzz');
+    });
+
+    // exercise 3: return "Not a FizzBuzz" when not called with a number divisible by both 3 and 5
+    it('returns "Not a FizzBuzz" when not called with a number divisible by 3 and 15', function () {
+        expect(fizzbuzz(1)).to.equal(1);
+    });
+
+    // exercise 3: return "Not a FizzBuzz" when not called with a number divisible by both 3 and 5
+    it('returns "Not Valid Input" if it is invalid input', function () {
+        expect(function(){ fizzbuzz({}) }).to.throw("Not Valid Input");
+    });
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,18 @@
 'use strict';
+var _ = require('lodash');
 
-module.exports = function () {
-    return 'Fizz';
+module.exports = function (fizzbuzz) {
+
+    if (_.isNumber(fizzbuzz) ) {
+
+        return fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0 ?
+            'FizzBuzz'
+            : fizzbuzz % 3 == 0
+            ? 'Fizz'
+            : fizzbuzz % 5 == 0
+            ? 'Buzz'
+            : fizzbuzz
+    } else {
+        throw new Error("Not Valid Input");
+    }
 };
