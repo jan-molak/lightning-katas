38084bf9ab029001ff609256f7686139441db299
Andy Washbrook <awashbrook@gmail.com>
2014-11-11 09:51:07 +0000

    Update defaults.spec.js

--- spec/defaults.spec.js
+++ spec/defaults.spec.js
@@ -3,10 +3,19 @@
 var expect    = require('chai').expect,
     configure = require('../src/configure');
 
+
 describe('configure', function () {
+
+
 
     it('returns a set of sensible defaults', function() {
         expect(configure()).to.deep.equal({ user: 'user', password: 'password', url: 'http://github.com' });
+    });
+
+    it('allows user to override defaults', function(){
+        var override = {user:'Bob',password: 'MyPassword'};
+
+        expect(configure(override)).to.deep.equal({user:'Bob',password:'MyPassword', url: 'http://github.com' });
     });
 
     // exercise 1: allow the user to override some defaults,
