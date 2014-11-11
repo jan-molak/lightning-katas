83f401368b9629e525f2f2889b9838f2c5ab4354
nmiddleton <nemes1s>
2014-11-11 11:23:01 +0000

    exercise configure defaults (minimum code)

--- spec/defaults.spec.js
+++ spec/defaults.spec.js
@@ -1,29 +1,37 @@
 'use strict';
 
-var expect    = require('chai').expect,
+var chai = require('chai'),
+    expect = chai.expect,
     configure = require('../src/configure');
 
 
 describe('configure', function () {
 
-
-
-    it('returns a set of sensible defaults', function() {
+    it('returns a set of sensible defaults', function () {
         expect(configure()).to.deep.equal({ user: 'user', password: 'password', url: 'http://github.com' });
     });
-
-    it('allows user to override defaults', function(){
-        var override = {user:'Bob',password: 'MyPassword'};
-
-        expect(configure(override)).to.deep.equal({user:'Bob',password:'MyPassword', url: 'http://github.com' });
-    });
-
     // exercise 1: allow the user to override some defaults,
     //             i.e. configure({ user: 'bob', password: 'secret' }) -> { user: 'bob', password: 'secret', url: 'http://github.com' }
     //
     //             hint: you can use lodash :-)
 
-    // exercise 2: try both _.merge and _.defaults
+    it('allows user to override defaults', function () {
+        var override = {user: 'Bob', password: 'MyPassword'};
+        expect(configure.byMerge(override)).to.deep.equal({user: 'Bob', password: 'MyPassword', url: 'http://github.com' });
+    });
 
+    describe('doing it with lodash', function () {
+        // exercise 2: try both _.merge and _.defaults
+        it('allow the user to override some defaults (using _.merge )', function () {
+            expect(configure.byMerge({ user: 'fred', password: 'wilma'})).to.deep.equal({ user: 'fred', password: 'wilma', url: 'http://github.com' });
+        });
+
+        it('allow the user to override some defaults (using _.defaults )', function () {
+            expect(configure.byDefaults({ user: 'fred', password: 'wilma'})).to.deep.equal({ user: 'fred', password: 'wilma', url: 'http://github.com' });
+        });
+    });
     // exercise 3: can you do it without lodash?
+    it('allow the user to override some defaults (without lodash)', function () {
+        expect(configure.byHand({ user: 'bob', password: 'secret' })).to.deep.equal({ user: 'bob', password: 'secret', url: 'http://github.com' });
+    });
 });
--- src/configure.js
+++ src/configure.js
@@ -1,9 +1,29 @@
 'use strict';
+var _ = require('lodash');
 
-module.exports = function (/* overrides */) {
-    return {
-        user:     'user',
-        password: 'password',
-        url:      'http://github.com'
-    };
+var default_profile =  {
+    user:     'user',
+    password: 'password',
+    url:      'http://github.com'
+};
+module.exports = function(){
+    return default_profile;
+};
+
+module.exports.byMerge = function (override) {
+   _.merge(default_profile,override);
+    return default_profile;
+};
+
+module.exports.byDefaults = function (override) {
+    _.defaults(default_profile,override);
+    return default_profile;
+};
+
+module.exports.byHand =  function (override) {
+    if (override) {
+        default_profile.user     = override.user || default_profile.user;
+        default_profile.password = override.password || default_profile.password;
+    }
+    return default_profile;
 };
